d3194ae531950d085d646cc09b38cdf6e26b3a2d
王春林 <wangchunlin@shuidihuzhu.com>
2021-09-10 14:35:18 +0800

    update

--- app/src/main/java/com/example/testfluttermodule/MainActivity.java
+++ app/src/main/java/com/example/testfluttermodule/MainActivity.java
@@ -1,18 +1,64 @@
 package com.example.testfluttermodule;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+public class MainActivity extends FlutterActivity {
+    public static final String CHANNEL = "samples.flutter.io/battery";
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                        if (call.method.equals("getBatteryLevel")) {
+                            int batteryLevel = getBatteryLevel();
+                            if (batteryLevel != -1) {
+                                result.success(batteryLevel);
+                            } else {
+                                result.error("UNAVAILABLE", "Battery level not available.", null);
+                            }
+                        } else {
+                            result.notImplemented();
+                        }
+                    }
+                });
 
-public class MainActivity extends AppCompatActivity {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
     }
 
     public void onJumpToFlutter(View view) {

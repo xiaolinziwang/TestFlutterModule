66076cedfb842ad7391070a7e506a6a74075c25d
王春林 <wangchunlin@shuidihuzhu.com>
2021-09-10 10:40:51 +0800

    成功跳转

--- app/src/main/java/com/example/testfluttermodule/MainActivity.java
+++ app/src/main/java/com/example/testfluttermodule/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testfluttermodule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onJumpToFlutter(View view) {
+        try {
+            startActivity(FlutterActivity.createDefaultIntent(this));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
